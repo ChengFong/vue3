063af6d405073459b5bfe2ef04a3072795f7a32b
DESKTOP-L2SS145\User <40125210@gm.nfu.edu.tw>
2024-07-29 08:52:00 +0800

    096_SKU模块静态的搭建

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>SKU管理</h1>
+  <el-card>
+    <el-table border>
+      <el-table-column
+        label="序號"
+        type="index"
+        align="center"
+        width="80px"
+      ></el-table-column>
+      <el-table-column
+        label="名稱"
+        show-overflow-tooltip
+        width="150px"
+      ></el-table-column>
+      <el-table-column
+        label="描述"
+        show-overflow-tooltip
+        width="150px"
+      ></el-table-column>
+      <el-table-column label="圖片" width="150px"></el-table-column>
+      <el-table-column label="重量" width="150px"></el-table-column>
+      <el-table-column label="價格" width="150px"></el-table-column>
+      <el-table-column
+        label="操作"
+        width="250px"
+        fixed="right"
+      ></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script setup lang="ts"></script>
