7a07aa6fe8939fbb29b0bbaec7cc4c06c0cdd54e
DESKTOP-L2SS145\User <40125210@gm.nfu.edu.tw>
2024-07-30 08:56:57 +0800

    099_商品详情静态搭建 0454

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -68,6 +68,20 @@
       @current-change="getHasSku"
       @size-change="handler"
     ></el-pagination>
+    <!-- 抽屜組件: 展示商品詳情 -->
+    <el-drawer v-model="drawer" title="I'm outer Drawer" size="50%">
+      <div>
+        <el-button @click="innerDrawer = true">Click me!</el-button>
+        <el-drawer
+          v-model="innerDrawer"
+          title="I'm inner Drawer"
+          :append-to-body="true"
+          :before-close="handleClose"
+        >
+          <p>_(:зゝ∠)_</p>
+        </el-drawer>
+      </div>
+    </el-drawer>
   </el-card>
 </template>
 
