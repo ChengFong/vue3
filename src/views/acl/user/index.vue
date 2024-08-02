82762f1648b5640b673fb5c693aa3a233e86f84a
LAPTOP-66STUFHM\Luster <bob@lustertech.net>
2024-08-02 18:35:04 +0800

    102_用户管理静态的搭建 1339

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <h1>用戶管理</h1>
+  <el-card style="height: 80px;">
+    <el-form :inline="true" class="form">
+      <el-form-item label="用戶名:">
+        <el-input placeholder="請你輸入搜索用戶名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="default">搜索</el-button>
+        <el-button type="primary" size="default">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <el-card style="margin: 10px 0px;">
+    123
+  </el-card>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.form {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
