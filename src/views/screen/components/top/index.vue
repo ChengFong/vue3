b92d6f404eed204c3339c2c2de740022943ae78a
LAPTOP-66STUFHM\Luster <bob@lustertech.net>
2024-08-20 17:51:48 +0800

    130_数据大屏顶部的搭建

---
+++ src/views/screen/components/top/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lbtn" @click="goHome">首頁</span>
+    </div>
+    <div class="center">
+      <div class="title">智慧旅遊可視化大數據平台</div>
+    </div>
+    <div class="right">
+      <span class="rbtn">統計報告</span>
+      <span class="time">當前時間: {{ time }}</span>
+    </div>
+  </div>
+</template>
+  
+<script setup lang="ts">
+// 點擊首頁按鈕回到首頁
+import { useRouter } from 'vue-router';
+import { ref, onMounted, onUnmounted } from 'vue';
+import moment from 'moment'
+
+// 獲取路由器對象
+let $router = useRouter()
+// 存儲當前時間
+let time = ref()
+let timer = ref(0)
+
+// 組件掛載完畢更新當前的事件
+onMounted(() => {
+  timer.value = setInterval(()=>{
+    time.value = moment().format('YYYY年MM月DD日 hh:mm:ss')
+  }, 1000)
+})
+onUnmounted(() => {
+  clearInterval(timer.value)
+})
+
+// 按鈕的點擊回調
+const goHome = () => {
+  $router.push('/home')
+}
+</script>
+  
+<style scoped lang="scss">
+  .top {
+    width: 100%;
+    height: 40px;
+    display: flex;
+    .left {
+      flex: 1.5;
+      background: url(../../images/dataScreen-header-left-bg.png) no-repeat;
+      background-size: cover;
+      .lbtn {
+        width: 150px;
+        height: 40px;
+        float: right;
+        background: url(../../images/dataScreen-header-btn-bg-l.png) no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        line-height: 40px;
+        color: #29fcff;
+        font-size: 20px;
+      }
+    }
+    .center {
+      flex: 2;
+      .title {
+        width: 100%;
+        height: 74px;
+        background: url(../../images/dataScreen-header-center-bg.png) no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        line-height: 74px;
+        color: #29fcff;
+        font-size: 30px;
+      }
+    }
+    .right {
+      flex: 1.5;
+      background: url(../../images/dataScreen-header-left-bg.png) no-repeat;
+      background-size: cover;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .rbtn {
+        width: 150px;
+        height: 40px;
+        float: left;
+        background: url(../../images/dataScreen-header-btn-bg-r.png) no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        line-height: 40px;
+        color: #29fcff;
+        font-size: 20px;
+      }
+      .time {
+        color: #29fcff;
+        font-size: 20px;
+      }
+    }
+  }
+</style>
+  
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,72 @@
 <template>
-  <h1>數據大平</h1>
+  <div class="container">
+    <!-- 數據大屏展示內容區域 -->
+    <div class="screen" ref="screen">
+      <div class="top"><Top/></div>
+      <div class="bottom">
+        <div class="left">左側</div>
+        <div class="center">中間</div>
+        <div class="right">右側</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+// 引入頂部的子組件
+import Top from './components/top/index.vue'
 
-<style scoped lang="scss"></style>
+// 獲取數據大屏展示內容盒子的DOM元素
+let screen = ref()
+
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+
+// 定義大屏縮放比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+
+// 監聽視口變化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
+
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url( ./images/bg.png) no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    transform-origin: left top;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .right {
+        flex: 1
+      }
+      .left {
+        flex: 1
+      }
+      .center {
+        flex: 2
+      }
+    }
+  }
+}
+</style>
