8d6786ab8558c55bd61541745ba83b92159fafcd
Bob <bob@lustertech.net>
2024-08-30 14:09:44 +0800

    132_水球图完成

--- src/views/screen/components/tourist/index.vue
+++ src/views/screen/components/tourist/index.vue
@@ -9,13 +9,60 @@
             <span v-for="(item, index) in people" :key="index">{{ item }}</span>
         </div>
         <!-- 盒子將來echarts展示圖形圖標的節點 -->
-         <div class="charts">123</div>
+         <div class="charts" ref="charts">123</div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts';
+// 水球圖擴展插件
+import 'echarts-liquidfill'
+
 let people = ref('215908人')
+// 獲取節點
+let charts = ref();
+
+onMounted(() => {
+    // 獲取echarts類的實例
+    let mycharts = echarts.init(charts.value);
+    // 設置實例的配置項
+    mycharts.setOption({
+        // 標題組件
+        title: {
+            text: '水球圖'
+        },
+        // x|y軸組件
+        xAxis: {},
+        yAxis: {},
+        // 系列: 決定你展示什麼樣的圖形圖標
+        series: {
+            type: 'liquidFill', // 系列
+            data: [0.6, 0.4, 0.2], // 展示的數據
+            waveAnimation: true, // 動畫
+            animationDuration: 3,
+            animationDurationUpdate: 0,
+            radius: '100%', // 半徑
+            outline: {
+                borderDistance: 0,
+                itemStyle: {
+                    borderWidth: 5,
+                    borderColor: '#156ACF',
+                    shadowBlur: 20,
+                    shadowColor: 'rgba(255, 0, 0, 1)'
+                }
+            }
+        },
+        // 布局組件
+        grid: {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+        }
+    })
+})
+
 </script>
 
 <style scoped lang="scss">
@@ -65,7 +112,6 @@ let people = ref('215908人')
     .charts {
         width: 100%;
         height: 250px;
-        background: red;
     }
 }
 </style>
